33eac45f6e76021b8ee818d686c7745152be736a
jinhui <dnekfl77@gmail.com>
2022-12-15 16:47:07 +0900

    Add plugin sample.

---
+++ pages/samples/plugins.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2 class="font-bold text-xl">Plugins sample</h2>
+  <div class="mt-3">
+    <p>{{ $hello('world') }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { $hello } = useNuxtApp();
+</script>
