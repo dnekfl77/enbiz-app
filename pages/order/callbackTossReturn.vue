1f3fed736b332db7dab101d86e43c5dff5e45ef6
jinhui <dnekfl77@gmail.com>
2023-02-09 15:38:17 +0900

    Toss Callback 결과 페이지 추가

---
+++ pages/order/callbackTossReturn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Token: {{ 1 + 1 }}</div>
+</template>
+
+<script lang="ts">
+import _ from 'lodash';
+
+definePageMeta({
+  layout: 'empty',
+});
+
+export default {
+  setup() {
+console.log('callback setup.');
+  }
+}
+</script>
